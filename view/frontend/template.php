f32b2e1bc48ee078b5a607865ac06ba033828965
cidja <chgeorgesc@gmail.com>
2019-12-28 18:53:12 +0100

    Update template.php

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -18,7 +18,7 @@
     <header>
         <!--Brand  source: https://www.w3schools.com/bootstrap4/bootstrap_navbar.asp !-->
             <nav class="navbar navbar-expand-md bg-success navbar-dark fixed-top">
-                <a class="navbar-brand" title="page d'accueil" href="index.php?action=home">Jean Forteroche</a>
+                <a class="navbar-brand" title="page d'accueil" href="index.php?action=home">Prête ton livre</a>
             <!-- Toggler / collapsible Button !-->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
                     <span class="navbar-toggler-icon"></span>
@@ -28,10 +28,10 @@
             <div class="collapse navbar-collapse justify-content-md-end align-items-sm-start" id="collapsibleNavbar">
                 <ul class="navbar-nav">
                 <li class="nav-item ml-1">
-                    <a class="nav-link" href="index.php?action=home">ACCUEIL</a>
+                    <a class="nav-link" href="index.php?action=home">C'est quoi</a>
                 </li>
                 <li class="nav-item ml-2">
-                    <a class="nav-link" href="index.php?action=listPosts">ARTICLES</a>
+                    <a class="nav-link" href="index.php?action=listPosts">Trouve ton livre</a>
                 </li>
                 <li class="nav-item">
                     <a id="adminLink" class="btn btn-secondary" title="login" href="index.php?action=formAdmin">CONNEXION</a>
