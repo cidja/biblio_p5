f2323f55821447f00540a032fe97ee35b7920664
cidja <chgeorgesc@gmail.com>
2020-01-05 20:55:20 +0100

    Update ManagerDb-serveur.php

--- model/ManagerDb-serveur.php
+++ model/ManagerDb-serveur.php
@@ -5,12 +5,11 @@ class ManagerDb
     protected function dbConnect()
 {
     //source: https://my.ionos.fr/pdo-extension/dbs224218
-    $host_name = 'db5000229643.hosting-data.io';
-    $database = 'dbs224218';
-    $user_name = 'dbu348423';
-    $password = '6C@ieJ+@72wG';
+    $host_name = 'db5000262470.hosting-data.io';
+    $database = 'dbs256083';
+    $user_name = 'dbu430244';
+    $password = 'Uv9U={d9C8(w';
     $dbh = null;
-
     try
     {
         $db = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
