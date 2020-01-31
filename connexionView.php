a68f6131c468970aec322c9227e526d1d860b17d
cidja <chgeorgesc@gmail.com>
2020-01-31 09:01:30 +0100

    fontfamilly connexionView input change

--- connexionView.php
+++ connexionView.php
@@ -22,11 +22,11 @@
         <form method="post" action="index.php?action=checkConnexion">
             <div class="form-group">
                 <label for="user">Utilisateur :</label>
-                <input type="text" class="form-control" id="user" name="user" autofocus required>
+                <input type="text" class="form-control user" id="user" name="user" autofocus required>
             </div>
             <div class="form-group">
                 <label for="pwd">Mot de passe : </label>
-                <input type="password" class="form-control" id="pwd" name="pwd" required >
+                <input type="password" class="form-control pwd" id="pwd" name="pwd" required >
             </div>
             <div class="form-group">
                 <input class="submitButtonConnexion +" type="submit" value="Valider">
