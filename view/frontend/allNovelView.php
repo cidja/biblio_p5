dc8983fbb4c5825f215c7cd33c598c5b188d8153
cidja <chgeorgesc@gmail.com>
2020-01-31 08:46:41 +0100

    Update allNovelView.php
    add title under cover in allNovel view

--- view/frontend/allNovelView.php
+++ view/frontend/allNovelView.php
@@ -22,6 +22,9 @@ ob_start(); //Start of capture to put it in the variable at the end of the scrip
                     <a href="index.php?action=oneNovel&amp;id=<?= $data["id"];?>">
                         <img class="imgCover +"src=<?=$cover; ?> alt="couverture du livre" title="couverture du livre" />
                     </a>
+                    <div class="">
+                        <div><?= $data["title"]; ?></div>
+                    </div>
                     <div class="mt-1">
                         <?php
                     if($data["finish"] == 0){
