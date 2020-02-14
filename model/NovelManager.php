e06cf71993695100b65343f800b2dd84a5b32a5b
cidja <chgeorgesc@gmail.com>
2020-02-14 09:57:50 +0100

    ajax input

--- model/NovelManager.php
+++ model/NovelManager.php
@@ -28,6 +28,16 @@ class Model_NovelManager extends Model_ManagerDb
             
         }
 
+        public function oneNovelInfosAjax($id) //method for retrieving all the information from one novel with $_GET["id"]
+        {
+            $db = $this->dbConnect();
+            $infos = $db->prepare('SELECT id,title, author, isbn, genre, `publication`, page_count, count_volume, active,finish, comment,rate,cover,
+                                    DATE_FORMAT(creation_date, "%d/%m/%Y à %Hh%imin%ss") AS creation_date_fr FROM novel WHERE id=?');
+            $infos->execute(array($id));
+            echo $infos;
+            
+        }
+
         /* NOT USE
         public function novelsRead() // method that displays the books read based on whether 1 in the table (read) otherwise (not finished)
         
