2b69dcbb506f501aac1a8906177f53e1543bc3f5
cidja <chgeorgesc@gmail.com>
2020-02-24 17:24:16 +0100

    navbar for connexion view

--- view/backend/formAccessUser.php
+++ view/backend/formAccessUser.php
@@ -1,20 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr-FR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Connexion superutilisateur</title>
-    <link rel="stylesheet" type="text/css" href="public/css/style.css" >
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Bangers|Marck+Script&display=swap" rel="stylesheet"> <!--google font !-->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/bad7172f0a.js" crossorigin="anonymous"></script> <!--cdn fontawesome source: https://fontawesome.com/kits/bad7172f0a/settings !-->
-    
-</head>
-    <body>
+<?php
+$title = "connexion membre";
+ob_start();
+?>
         <section class="container text-center jumbotron connexion mt-5">
             <form method="post" action="index.php?action=checkMember">
                 <h3>Accès membre :</h3>
@@ -30,6 +17,7 @@
                     <input class="submitButtonConnexion +" type="submit" value="Valider">
                 </div>
             </form>
-
-    </body>
-</html>
+    <?php
+        $content = ob_get_clean();
+        require("templateConnexion.php");
+    ?>
--- view/backend/formNewUser.php
+++ view/backend/formNewUser.php
@@ -1,21 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr-FR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Création nouvel utilisateur</title>
-    <link rel="stylesheet" type="text/css" href="public/css/style.css" >
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Bangers|Marck+Script&display=swap" rel="stylesheet"> <!--google font !-->
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/bad7172f0a.js" crossorigin="anonymous"></script> <!--cdn fontawesome source: https://fontawesome.com/kits/bad7172f0a/settings !-->
-</head>
-    <body>
-        <div class="container text-center">
-            <h2>Création d'un nouvel utilisateur :</h2>
+<?php
+
+$title = "Inscription";
+ob_start();
+?>
+        <div class="container text-center jumbotron">
+            <h2 class="text-uppercase">Création d'un nouvel utilisateur :</h2>
             <div class="formUser">
                 <form action="/p5/index.php?action=createUserConfirm" method="post">
                     <div class="form-group">
@@ -38,5 +27,7 @@
             </div>
         </div>
         <script src="/p5/public/js/form_password_check.js"></script>
-    </body>
-</html>
+    <?php
+    $content = ob_get_clean();
+    require("templateConnexion.php");
+    ?>
