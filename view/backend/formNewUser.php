1cb7d8099dae7c2bc9ade70f0e7c4bd50b30b210
cidja <chgeorgesc@gmail.com>
2020-02-24 15:43:10 +0100

    Update formNewUser.php
    check password ok

--- view/backend/formNewUser.php
+++ view/backend/formNewUser.php
@@ -24,18 +24,19 @@
                     </div>
                     <div class="form-group">
                         <label for="password1">Mot de passe : </label>
-                        <input type="password" class="form-control" id="password1" name="password1">
+                        <input type="password" class="form-control field" id="pwd1" name="password1">
                     </div>
                     <div class="form-group">
                         <label for="password2">Retaper le mot de passe : </label>
-                        <input type="password" class="form-control" id="password2" name="password2">
+                        <input type="password" class="form-control field" id="pwd2" name="password2">
                     </div>
                     <div class="form-group">
-                        <input class="btn btn-info" type="submit" value="valider">
+                        <input class="btn btn-info" type="submit" value="valider" id="submitButton">
                     </div>
                 </form>
+                <div id="result"></div>
             </div>
         </div>
-
+        <script src="/p5/public/js/form_password_check.js"></script>
     </body>
 </html>
