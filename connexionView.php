3e4865af79721714cb95a0802d1d0658d4a16671
cidja <chgeorgesc@gmail.com>
2020-02-29 18:09:59 +0100

    add favicon

--- connexionView.php
+++ connexionView.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Connexion</title>
+    <link rel="icon" href="public/img/favicon.ico" >
     <link rel="stylesheet" type="text/css" href="public/css/style.css" >
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Bangers|Marck+Script&display=swap" rel="stylesheet"> <!--google font !-->
